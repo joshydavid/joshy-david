09995260ce72bf3dd15e73f750114d616be80cc3
Joshua David <joshuadavidang@outlook.sg>
2023-12-08 09:13:56 +0800

    add og banner (#12)
    * feat: add og banner
    * fix: rename img file name
    * docs: update read me

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,9 +1,9 @@
-import Sayless from "@/public/projects/Sayless-All.png";
-import CodingPortfolio01 from "@/public/projects/Coding-Portfolio-v1.png";
-import Client01 from "@/public/projects/Client01.png";
-import Sayocode from "@/public/projects/Sayocode.png";
-import CodingPortfolio02 from "@/public/projects/Coding-Portfolio-v2.png";
-import v2Portfolio from "@/public/projects/v2Portfolio.png";
+import Sayocode from "@/public/projects/sayocode.png";
+import LegacyCodingPortfolio from "@/public/projects/legacy-portfolio.png";
+import Client from "@/public/projects/client.png";
+import Sayless from "@/public/projects/sayless-ecosystem.png";
+import CodingPortfolio from "@/public/projects/portfolio-2.0.png";
+import CodingPortfolioAll from "@/public/projects/portfolio-2.0-ecosystem.png";
 
 type Projects = {
   id: number;
@@ -27,12 +27,12 @@ export const projects: Projects[] = [
   },
   {
     id: 2,
-    name: "Coding Portfolio 1.0",
-    description: "Coding portfolio.",
+    name: "Coding Portfolio",
+    description: "Coding portfolio (Legacy)",
     detailedDescription:
       "Designed and developed a responsive coding portfolio, built on top of Next.js & Tailwind CSS, deployed to Vercel.",
     year: 2022,
-    icon: CodingPortfolio01,
+    icon: LegacyCodingPortfolio,
     link: "https://github.com/joshuadavidang/joshua-david",
   },
   {
@@ -40,7 +40,7 @@ export const projects: Projects[] = [
     name: "Custom Web App",
     description: "Developed a custom personal brand web app for clients.",
     year: 2023,
-    icon: Client01,
+    icon: Client,
     link: "https://celineongjw.com",
   },
   {
@@ -60,8 +60,8 @@ export const projects: Projects[] = [
     detailedDescription:
       "Version 2.0 - Redesigned and redeveloped an updated version of my coding portfolio, built on top of Next.js 14.0, Tailwind CSS, DaisyUI, HeadlessUI deployed to Vercel.",
     year: 2024,
-    icon: CodingPortfolio02,
-    detailedImage: v2Portfolio,
+    icon: CodingPortfolio,
+    detailedImage: CodingPortfolioAll,
     link: "https://github.com/joshuadavidang/joshuadavid",
   },
 ];
