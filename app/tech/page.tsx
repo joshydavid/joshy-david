e1a188035c36ff8d1d86e08712cc077f0804ad81
Joshua David <joshuadavidang@outlook.sg>
2023-12-24 19:14:59 +0800

    feat: update animation for project/tech page (#30)

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -4,22 +4,29 @@ import Image from "next/image";
 
 export default function Tech() {
   return (
-    <AnimatedSection delay={0.1} className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3">
       <h1>Tech</h1>
       <h4 className="mb-4">Some of my favourite tech products.</h4>
-      {techProducts.map(({ id, product, icon }, index) => (
-        <div className="avatar flex items-center gap-8" key={index}>
-          <div className="w-24 rounded-xl">
-            <Image
-              src={icon}
-              alt={`Product ${id}`}
-              className="dark:bg-white bg-lightGray hover:opacity-80"
-              priority
-            />
-          </div>
-          <p className="text-sm">{product}</p>
-        </div>
-      ))}
-    </AnimatedSection>
+      {techProducts.map(({ id, product, icon }, delay) => {
+        delay *= 0.1;
+        return (
+          <AnimatedSection
+            className="avatar flex items-center gap-8"
+            key={id}
+            delay={delay}
+          >
+            <div className="w-24 rounded-xl">
+              <Image
+                src={icon}
+                alt={`Product ${id}`}
+                className="dark:bg-white bg-lightGray hover:opacity-80"
+                priority
+              />
+            </div>
+            <p className="text-sm">{product}</p>
+          </AnimatedSection>
+        );
+      })}
+    </div>
   );
 }
