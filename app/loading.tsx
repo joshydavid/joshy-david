8716b89212127c5773fb9095206ba988b143b30b
Joshua David <joshuadavidang@outlook.sg>
2024-01-03 19:35:59 +0800

    fix: fallback loading icon (#43)

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+
 export default function Loading() {
   return (
     <div className="flex items-center justify-center w-screen">
-      <span className="loading loading-dots loading-lg"></span>
+      <DotsHorizontalIcon className="w-14 h-auto animate-pulse" />
     </div>
   );
 }
