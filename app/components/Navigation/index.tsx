f93f731db8ed30a52da77e91ac4f4da49ae88ae7
Joshua David <joshuadavidang@outlook.sg>
2024-04-09 18:01:22 +0800

    fix: use 12h format (#61)
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -5,7 +5,7 @@ import { format } from "date-fns";
 
 export default function Navigation() {
   const date = new Date();
-  const formattedDate = format(date, "EEE HH:mm a");
+  const formattedDate = format(date, "EEE hh:mm a");
 
   return (
     <div className="flex flex-row gap-4 bg-white dark:bg-black w-full justify-between px-6 lg:px-0 lg:justify-evenly items-center pt-12">
