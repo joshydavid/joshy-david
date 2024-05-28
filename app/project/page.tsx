86cad5c11130d92e4a304994caa56ccfd6f27762
Joshua David <joshuadavidang@outlook.sg>
2024-05-28 13:01:33 +0800

    fix: code clean up (#88)
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -22,27 +22,25 @@ export default function Project() {
       <h4 className="mb-4">Projects I have done over the years.</h4>
       {sortedProjects.map(({ id, slug, name, icon, tags }: ProjectType) => {
         return (
-          <div key={id}>
-            <Link href={`/project/${slug}`}>
-              <div className="flex flex-col md:flex-row md:items-center items-start gap-8 mb-12 md:mb-3 cursor-pointer hover:opacity-90 dark:bg-black dark:text-white rounded-xl">
-                <Image
-                  src={icon}
-                  alt={name}
-                  className="bg-lightGray dark:bg-gray rounded-xl p-5 md:w-1/3 w-screen"
-                  priority
-                />
+          <Link href={`/project/${slug}`} key={id}>
+            <div className="flex flex-col md:flex-row md:items-center items-start gap-8 mb-12 md:mb-3 cursor-pointer dark:bg-black dark:text-white rounded-xl hover:opacity-90">
+              <Image
+                src={icon}
+                alt={name}
+                className="bg-lightGray dark:bg-gray rounded-xl p-5 md:w-1/3 w-screen"
+                priority
+              />
 
-                <div className="flex flex-col gap-2 dark:text-lightGray">
-                  <h2>{name}</h2>
-                  <div className="flex gap-2">
-                    {tags.map((tag, index) => (
-                      <Badge key={index}>{tag}</Badge>
-                    ))}
-                  </div>
+              <div className="flex flex-col gap-2 dark:text-lightGray">
+                <h2>{name}</h2>
+                <div className="flex gap-2">
+                  {tags.map((tag, index) => (
+                    <Badge key={index}>{tag}</Badge>
+                  ))}
                 </div>
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
         );
       })}
     </div>
