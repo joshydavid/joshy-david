a1545a85e08cb09e8fd9fab5a98c3d0220dab553
Joshua David <joshuadavidang@outlook.sg>
2024-06-08 19:36:15 +0800

    feat: new loading spinner (#97)
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,10 @@
-import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function Loading() {
   return (
-    <div className="flex w-screen items-center justify-center">
-      <DotsHorizontalIcon className="h-auto w-14 animate-pulse" />
+    <div className="flex w-screen items-center justify-center gap-4">
+      <ClipLoader size="30" color="white" speedMultiplier={0.5} />{" "}
+      <h2>Loading...</h2>
     </div>
   );
 }
