315403883b502f0f8d6bcb8bbb56340cf1897623
joshydavid <joshuadavidang@outlook.sg>
2024-06-26 23:57:56 +0800

    feat: add in mentorship experience
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

---
+++ app/data/mentorship.ts
@@ -0,0 +1,13 @@
+import { Mentorship } from "@/models/mentorship";
+import Hack from "@/public/companies/Hack.png";
+
+export const mentorships: Mentorship[] = [
+  {
+    id: 1,
+    organisation: "SMU .Hack",
+    position: "SMU HEAP, Software Engineer Mentor",
+    timeline: "May 2024 - July 2024",
+    icon: Hack,
+    link: "https://www.linkedin.com/company/smuhack/",
+  },
+];
