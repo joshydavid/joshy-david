fc6d96a8b03a6e4c37da3878f492120f865f3593
Joshua David <joshuadavidang@outlook.sg>
2024-06-28 21:41:33 +0800

    fix: ui gap (#118)
    * fix: update companany logos
    
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>
    
    * fix: internship column gap
    
    * fix: ui fixes
    
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>
    
    ---------
    
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex w-full flex-col items-center gap-2 md:items-start">
-      <h1 className="text-red-600 dark:text-red-500">Page Not Found</h1>
+    <div className="flex w-full flex-col items-center gap-1 pt-6">
+      <h2 className="text-red-600 dark:text-red-500">Page Not Found</h2>
       <p>Could not find requested resource.</p>
       <Link href="/">
         Return
-        <span className="pl-1.5 underline dark:hover:text-lightGray">Home</span>
+        <span className="pl-1.5 underline underline-offset-4 dark:hover:text-lightGray">
+          Home
+        </span>
       </Link>
     </div>
   );
