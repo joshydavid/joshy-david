28c2a68d0e19d2dd8f124acac3dcb2489d7b483c
joshydavid <joshuadavidang@outlook.sg>
2024-06-30 11:50:18 +0800

    fix: remove unused components
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

--- app/constant/index.tsx
+++ app/constant/index.tsx
@@ -1,5 +1,6 @@
 export const NAME = "Joshua David";
 export const POSITION = "Software Engineer";
 export const RESUME_URL = "/Joshua_David.pdf";
+export const HEADLINE = "Looking for Summer Internship (2025)";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/joshydavid";
 export const GITHUB_URL = "https://github.com/joshydavid";
