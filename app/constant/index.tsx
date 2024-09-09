3e4573a0a62580ad7ed8d7a7abc54291ef44ee83
joshydavid <joshuadavidang@outlook.sg>
2024-09-09 12:16:47 +0800

    chore: update resume and title
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

--- app/constant/index.tsx
+++ app/constant/index.tsx
@@ -1,6 +1,6 @@
 export const NAME = "Joshua David";
 export const POSITION = "Software Engineer";
-export const RESUME_URL = "/Joshua_David.pdf";
-export const HEADLINE = "Looking for Summer Internship (2025)";
+export const RESUME_URL = "/Joshua_David_Resume.pdf";
+export const HEADLINE = "Available for Software Engineering Internship (2025)";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/joshydavid";
 export const GITHUB_URL = "https://github.com/joshydavid";
