96626f60161b7efa417cea941c9b4fa847560075
Joshua David <joshuadavidang@outlook.sg>
2024-11-07 08:57:21 +0800

    refactor: update bio, abstract it from UI (#147)
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

--- app/constant/index.tsx
+++ app/constant/index.tsx
@@ -5,3 +5,5 @@ export const RESUME_URL =
 export const HEADLINE = "Available for Software Engineering Internship (2025)";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/joshydavid";
 export const GITHUB_URL = "https://github.com/joshydavid";
+export const BIO =
+  "Hey, I'm Joshua! I'm passionate about all things software engineering, with a strong interest in building complex and large-scale distributed systems. I'm currently picking up Swift to develop apps for iOS and visionOS  - feel free to reach out!";
