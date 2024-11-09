ca05699581e85a126ae98c0c48158d7a8635ef79
joshydavid <joshuadavidang@outlook.sg>
2024-11-09 09:10:07 +0800

    fix: update favicon and bio
    Signed-off-by: joshydavid <joshuadavidang@outlook.sg>

--- app/constant/index.tsx
+++ app/constant/index.tsx
@@ -6,4 +6,4 @@ export const HEADLINE = "Available for Software Engineering Internship (2025)";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/joshydavid";
 export const GITHUB_URL = "https://github.com/joshydavid";
 export const BIO =
-  "Hey, I'm Joshua! I'm passionate about all things software engineering, with a strong interest in building complex and large-scale distributed systems. I'm currently picking up Swift to develop apps for iOS and visionOS  - feel free to reach out!";
+  "Hey, I'm Joshua! I'm passionate about all things software engineering, with a strong interest in building complex and large-scale distributed systems. I'm currently picking up Swift to develop apps for iOS and visionOS  — feel free to reach out!";
