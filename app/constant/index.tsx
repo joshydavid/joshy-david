62f1de64f9c8d5cbc31a1e59f0c360120d8b78dd
joshydavid <cxang.2022@scis.smu.edu.sg>
2024-12-01 19:51:12 +0800

    feat: update resume url to cloudfront address
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/constant/index.tsx
+++ app/constant/index.tsx
@@ -1,7 +1,7 @@
 export const NAME = "Joshua David";
 export const POSITION = "Software Engineer";
 export const RESUME_URL =
-  "https://joshydavid.s3.amazonaws.com/Joshua_David_Resume.pdf";
+  "https://dhqsl2r2qz28p.cloudfront.net/Joshua_David_Resume.pdf";
 export const HEADLINE = "Available for Software Engineering Internship (2025)";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/joshydavid";
 export const GITHUB_URL = "https://github.com/joshydavid";
