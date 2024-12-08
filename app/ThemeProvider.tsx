5e396c4e0407d4bd4561a750c06d6662c45ab1e2
Joshua David <joshuadavidang@outlook.sg>
2024-12-08 20:05:57 +0800

    fix: use enum instead of hardcoded theme values (#175)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -2,10 +2,11 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ThemeProviderProps } from "next-themes/dist/types";
+import { Theme } from "./constant";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider themes={["light", "dark"]} {...props}>
+    <NextThemesProvider themes={[Theme.LIGHT, Theme.DARK]} {...props}>
       {children}
     </NextThemesProvider>
   );
