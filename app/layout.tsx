5aa1704b382c6def5f54b1ffb9b9c94937ab17d7
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-03-13 19:44:28 +0800

    feat: add tanstack query
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
-import { ThemeProvider } from "./ThemeProvider";
 import Navigation from "./components/Navigation";
 import { BIO, EXTERNAL_LINKS, Theme } from "./constant";
 import "./globals.css";
+import { TanStackProvider } from "./TanStackProvider";
+import { ThemeProvider } from "./ThemeProvider";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -35,12 +36,14 @@ export default function RootLayout({ children }: Readonly<ChildrenProps>) {
       suppressHydrationWarning={true}
     >
       <body className="bg-white dark:bg-stone-950">
-        <ThemeProvider attribute="class" defaultTheme={Theme.DARK}>
-          <Navigation />
-          <div className="flex min-h-screen bg-white px-6 py-8 pb-24 text-black dark:bg-stone-950 dark:text-white md:mx-auto md:w-1/2 md:pt-12 lg:w-1/3">
-            {children}
-          </div>
-        </ThemeProvider>
+        <TanStackProvider>
+          <ThemeProvider attribute="class" defaultTheme={Theme.DARK}>
+            <Navigation />
+            <div className="flex min-h-screen bg-white px-6 py-8 pb-24 text-black dark:bg-stone-950 dark:text-white md:mx-auto md:w-1/2 md:pt-12 lg:w-1/3">
+              {children}
+            </div>
+          </ThemeProvider>
+        </TanStackProvider>
       </body>
     </html>
   );
