1c5f5253dc002130b1f8930776b76680916f7504
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-03-15 20:57:27 +0800

    fix: hydration error (#236)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -2,14 +2,20 @@
 
 import ToggleSwitch from "@/components/ToggleSwitch";
 import { format } from "date-fns";
+import { useEffect, useState } from "react";
 
 export default function Navigation() {
-  const date = new Date();
-  const formattedDate = format(date, "EEE hh:mm a");
+  const [currDate, setCurrDate] = useState<string>();
+
+  useEffect(() => {
+    const date = new Date();
+    const formattedDate = format(date, "EEE hh:mm a");
+    setCurrDate(formattedDate);
+  }, []);
 
   return (
     <div className="flex w-full flex-row items-center justify-between gap-4 bg-white px-6 pt-12 dark:bg-stone-950 lg:justify-evenly lg:px-0">
-      <p>{formattedDate}</p>
+      <p>{currDate}</p>
       <ToggleSwitch />
     </div>
   );
