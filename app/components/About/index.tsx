f97e5be0f4a8875a5d85f8a5b3e7d3086982eb22
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-03-15 19:41:12 +0800

    fix: follow DRY principle (#235)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/components/About/index.tsx
+++ app/components/About/index.tsx
@@ -1,33 +1,43 @@
 "use client";
 
-import { retrieveBio } from "@/api/retrieveBio";
-import { retrieveInternships } from "@/api/retrieveInternship";
+import { getRequest } from "@/api/getRequest";
 import { BioSkeleton, ExperienceSkeleton } from "@/components/Skeletons";
+import { APIs } from "@/constant";
 import Project from "@/project/page";
 import { useQuery } from "@tanstack/react-query";
 import Bio from "./Bio";
 import InternshipExperience from "./InternshipExperience";
 import MentorshipExperience from "./MentorshipExperience";
-import { retrieveMentorships } from "@/api/retrieveMentorship";
 
 export default function About() {
+  const { BIO_AWS, INTERNSHIP_AWS, MENTORSHIP_AWS } = APIs;
+
   const {
     data: bioData,
     error: bioError,
     isLoading: bioLoading,
-  } = useQuery({ queryKey: ["bioData"], queryFn: retrieveBio });
+  } = useQuery({
+    queryKey: ["bioData"],
+    queryFn: () => getRequest(BIO_AWS),
+  });
 
   const {
     data: internshipData,
     error: internshipError,
     isLoading: internshipLoading,
-  } = useQuery({ queryKey: ["internshipData"], queryFn: retrieveInternships });
+  } = useQuery({
+    queryKey: ["internshipData"],
+    queryFn: () => getRequest(INTERNSHIP_AWS),
+  });
 
   const {
     data: mentorshipData,
     error: mentorshipError,
     isLoading: mentorshipLoading,
-  } = useQuery({ queryKey: ["mentorshipData"], queryFn: retrieveMentorships });
+  } = useQuery({
+    queryKey: ["mentorshipData"],
+    queryFn: () => getRequest(MENTORSHIP_AWS),
+  });
 
   const isLoading = bioLoading || internshipLoading || mentorshipLoading;
   const error = bioError || internshipError || mentorshipError;
