412f17f3151eaf8e7c8d6fdd393092b47cafd62c
Joshua David <cxang.2022@scis.smu.edu.sg>
2025-03-20 12:20:08 +0800

    fix: code clean up (#240)
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,9 +20,7 @@ const config = {
     extend: {
       colors: {
         ashGray: "#F3F4F6",
-        brightSun: "#FCD535",
         lightGray: "#FAFAFA",
-        skyBlue: "#26DDF9",
         gray: "#1F1D1D",
         green: "#16A34A",
         border: "hsl(var(--border))",
