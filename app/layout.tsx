bfccc56a8a210a80c630b32867b5f3909a9bf1ae
joshydavid <cxang.2022@scis.smu.edu.sg>
2025-07-23 12:03:38 +0800

    chore: add speed insights
    Signed-off-by: joshydavid <cxang.2022@scis.smu.edu.sg>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import { ReactNode } from "react";
@@ -45,6 +46,7 @@ export default function RootLayout({ children }: Readonly<ChildrenProps>) {
             </div>
           </ThemeProvider>
         </TanStackProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
